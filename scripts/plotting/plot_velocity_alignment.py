799089702b577ccb20affff49a3f2af0645587ee
Iryna Butsky <irynabutsky@MacBook-Pro.local>
2020-09-21 21:19:05 -0700

    updating plot_velocity_alignment

--- scripts/plotting/plot_velocity_alignment.py
+++ scripts/plotting/plot_velocity_alignment.py
@@ -21,24 +21,24 @@ sns.set_style("ticks",{'axes.grid': True, "ytick.major.size": 0.1,
 ion_list = ['H I', 'Si II', 'C III', 'O VI']
 color_list = ['gray', 'black', 'black', 'red']
 
-spec_list = glob.glob('../../data/analyzed_spectra/COS-FUV*')
+spec_list = glob.glob('../../data/analyzed_spectra/COS-FUV_P0*')
 for spec_path in spec_list:
-    plot_name = '%s/velocity.png'%(spec_path)
+    spec = os.path.basename(spec_path)
+    plot_name = '../../plots/velocity/%s.png'%(spec)
     if not os.path.isfile(plot_name):
-        spec = os.path.basename(spec_path)
         print(spec)
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 3.8))
+        ax.set_xlim(-300, 300)
+        ax.set_ylim(-0.05, 1.3)
+        ax.set_xlabel('Relative Velocity (km/s)')
+        ax.set_ylabel('Normalized Flux')
         for ion, color in zip(ion_list, color_list):
             vv, flux, vvfit, fluxfit,  wl, wlfit, w0 = spa.load_velocity_data(ion, spec)
             #ax.plot(vv, flux, color = 'black', alpha = 0.2)
             if os.path.isfile('%s/FitInspection.fits'%(spec_path)):
                 ax.plot(vvfit, fluxfit, color = 'black', linewidth = 4, alpha = 0.7)
                 ax.plot(vvfit, fluxfit, color = color, label = ion, linewidth = 2, alpha = 0.8)
-        ax.set_xlim(-300, 300)
-        ax.set_ylim(-0.05, 1.3)
-        ax.set_xlabel('Relative Velocity (km/s)')
-        ax.set_ylabel('Normalized Flux')
-        ax.legend(frameon = True)
-        fig.tight_layout()
-        plt.savefig(plot_name, dpi = 300)
+                ax.legend()
+                fig.tight_layout()
+                plt.savefig(plot_name, dpi = 300)
 
